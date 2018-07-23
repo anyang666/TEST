2eeb18a50db742d79be1507bad934b8caf5e926a
anyang <anyang@baidu.com>
2018-07-23 21:48:23 +0800

    update 22

--- gitTest/src/Main.java
+++ gitTest/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1111111");
+        System.out.println("2222222222");
     }
 }
